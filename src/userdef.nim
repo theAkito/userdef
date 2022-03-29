89d1d0b8cd5ff7e582ca9e4ed97047a1a25f37fe
Akito <the@akito.ooo>
2022-03-29 22:33:49 +0200

    Add Master Module

--- src/userdef.nim
+++ src/userdef.nim
@@ -1,4 +1,30 @@
+import
+  userdef/[
+    meta,
+    utils
+  ],
+  std/[
+    logging,
+    strutils,
+    os,
+    osproc,
+    posix,
+    sequtils
+  ]
 
+let logger = newConsoleLogger(defineLogLevel(), logMsgPrefix & logMsgInter & "master" & logMsgSuffix)
+
+proc run() =
+  let
+    passwdContent = utils.readLines(passwdPath)
+    groupContent = utils.readLines(groupPath)
+    passwdContentClean = passwdContent.filterNotContains("git")
+    groupContentClean = groupContent.filterNotContains("git")
+  passwdPath.writeFile(passwdContentClean.join(lineEnd))
+  groupPath.writeFile(groupContentClean.join(lineEnd))
+  #[ Initialise configuration file. ]#
+  #if not initConf(configPath): raise OSError.newException("Config file could neither be found nor generated!")
 
 when isMainModule:
-  echo "Greetings!"
+  # logger.log(lvlInfo, "Starting with the following configuration:\n" & pretty(%* config))
+  run()
