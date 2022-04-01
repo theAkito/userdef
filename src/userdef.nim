cdf5f42ccfe1d3458759f1434c464eba67e394cb
Akito <the@akito.ooo>
2022-04-01 16:45:15 +0200

    Re-Vamp Error Handling & Provide Help on App Usage

--- src/userdef.nim
+++ src/userdef.nim
@@ -2,7 +2,8 @@ import
   userdef/[
     meta,
     configurator,
-    usermanager
+    usermanager,
+    appinstructions
   ],
   std/[
     options,
@@ -35,11 +36,9 @@ proc raiseIfNotExists(filePath: string) =
   if not filePath.fileExists:
     raise OSError.newException("Path to config file provided is not a real file or cannot be found! Path to file provided: " & filePath)
 
-proc setOpts() =
-  for kind, key, val in commandLineParams().getopt():
+proc setOpts(params = commandLineParams()) =
+  for kind, key, val in params.getopt():
     case kind
-      of cmdArgument:
-        optSetConfigPath()
       of cmdLongOption, cmdShortOption:
         case key
           of "c", "config":
@@ -68,35 +67,43 @@ proc setOpts() =
               gid = val.parseInt
             except ValueError:
               raise ValueError.newException("GID provided is not a valid number!")
+      of cmdArgument:
+        optSetConfigPath()
       of cmdEnd: assert(false)
 
 proc run() =
-  #[ Initialise configuration file. ]#
-  if commandLineParams().len < 3:
-    thisConfigPath.raiseIfNotExists
-    if not initConf(thisConfigPath):
-      raise OSError.newException("Config file could neither be found nor generated! A config file is mandatory, if not all necessary user info is provided as arguments to this app.")
-  home = config.userdef.home.get("")
-  name = config.userdef.name.get("")
-  uid  = config.userdef.uid.get(invalidId)
-  gid  = config.userdef.gid.get(invalidId)
-  long = config.userdef.long.get(false)
-  #[ CLI arguments overwrite configuration options, as arguments are more ad-hoc in their nature than a configuration file. ]#
-  setOpts()
-  if home.isEmptyOrWhitespace or name.isEmptyOrWhitespace or uid == invalidId:
-    raise OSError.newException("Neither the configuration file nor the arguments provided were sufficient! You need to at least provide the custom user's home directory, name and UID!")
-  if gid == invalidId: gid = uid
-  name.deleteUser()
-  if long:
-    logger.log(lvlDebug, "Adding user manually...")
-    addUserMan(name, uid, gid, home)
-  else:
-    logger.log(lvlDebug, "Adding user officially...")
-    addUser(name, uid, gid, home)
-  logger.log(lvlDebug, "Passwd File:\n" & passwdPath.readFile)
-  logger.log(lvlDebug, "Shadow File:\n" & shadowPath.readFile)
-  logger.log(lvlDebug, "Group  File:\n" & groupPath.readFile)
+  try:
+    let params = commandLineParams()
+    if params.contains("--help"):
+      showHelp()
+      quit(0)
+    if params.len < 3:
+      thisConfigPath.raiseIfNotExists
+      if not initConf(thisConfigPath): #[ Initialise configuration file. ]#
+        raise OSError.newException("Config file could neither be found nor generated! A config file is mandatory, if not all necessary user info is provided as arguments to this app.")
+    home = config.userdef.home.get("")
+    name = config.userdef.name.get("")
+    uid  = config.userdef.uid.get(invalidId)
+    gid  = config.userdef.gid.get(invalidId)
+    long = config.userdef.long.get(false)
+    #[ CLI arguments overwrite configuration options, as arguments are more ad-hoc in their nature than a configuration file. ]#
+    params.setOpts()
+    if home.isEmptyOrWhitespace or name.isEmptyOrWhitespace or uid == invalidId:
+      raise OSError.newException("Neither the configuration file nor the arguments provided were sufficient! You need to at least provide the custom user's home directory, name and UID!")
+    if gid == invalidId: gid = uid
+    name.deleteUser()
+    if long:
+      logger.log(lvlDebug, "Adding user manually...")
+      addUserMan(name, uid, gid, home)
+    else:
+      logger.log(lvlDebug, "Adding user officially...")
+      addUser(name, uid, gid, home)
+    logger.log(lvlDebug, "Passwd File:\n" & passwdPath.readFile)
+    logger.log(lvlDebug, "Shadow File:\n" & shadowPath.readFile)
+    logger.log(lvlDebug, "Group  File:\n" & groupPath.readFile)
+  except:
+    logger.log(lvlFatal, getCurrentExceptionMsg())
+    showHelp()
 
 when isMainModule:
-  logger.log(lvlNotice, "Starting with the following configuration:\n" & pretty(%* config))
   run()
